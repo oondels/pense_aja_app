f2c62ef1538f41195b8e8fbf7e709079c33b3583
oondels <hendriusfelix@gmail.com>
2025-03-29 11:42:59 -0300

    configuracao banco de dados com docker

--- server/config/connection.php
+++ server/config/connection.php
@@ -3,20 +3,22 @@ require __DIR__ . '/../../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$host   = $_ENV['DB_HOST'];
-$user   = $_ENV['DB_USER'];
-$pass   = $_ENV['DB_PASS'];
-$dbname = $_ENV['DB_NAME'];
-$port   = $_ENV['DB_PORT'];
+// $host   = $_ENV['DB_HOST'];
+// $user   = $_ENV['DB_USER'];
+// $pass   = $_ENV['DB_PASS'];
+// $dbname = $_ENV['DB_NAME'];
+// $port   = $_ENV['DB_PORT'];
+
+$dsn = 'pgsql:host=pense_aja-database;port=5432;dbname=sest;';
+$user = 'postgres';
+$password = 'wa0i4Ochu#@*';
 
 //Conexão com a banco de dados
 try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
-    /*echo "Conexão com banco de dados realizado com sucesso.";
-    $check = true;
-    echo 'Try'.$check;*/
+    $conn = new PDO($dsn, $user, $password);
+    // echo "Conexão com banco de dados realizado com sucesso.";
+    // $check = true;
+    // echo 'Try'.$check;
 } catch (PDOException $err) {
-    echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $err->getMessage();
-    $check = false;
-    echo 'Catch' . $check;
+    echo 'Conexão falhou: ' . $err->getMessage();
 }
